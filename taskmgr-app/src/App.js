569b86c1621c58db1489688581d944f6c52fe192
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-31 09:34:15 -0400

    App.js edited. input, add btn, clear btn

--- taskmgr-app/src/App.js
+++ taskmgr-app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Live update ...1</p>
-      </header>
-    </div>
+    <>
+      <h2>To-Do List</h2> 
+      <input></input> {" "}
+      <button>Add</button> {" "}
+      <a>Clear</a>
+    </>
   );
 }
 
