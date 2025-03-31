2dd3a1088977a7a3869f4eedb5ee884678be0dfa
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-31 10:55:49 -0400

    useRef installed

--- taskmgr-app/src/components/Home.js
+++ taskmgr-app/src/components/Home.js
@@ -1,10 +1,30 @@
+import { useRef,useState } from "react"
+
+
+const initial_todo_items = [
+    {id:1,name:"item one", completed:false},
+    {id:2,name:"item two", completed:false},
+    {id:3,name:"item three", completed:true},
+    {id:4,name:"item four", completed:false},
+    {id:5,name:"item five", completed:false}
+    ];
+     
+const divDisplay = useRef("")
+
+divDisplay.innerHTML = initial_todo_items.map(item => `<p>${item.name}</p>`).join('');
+
+
 function Home() {
     return (
       <>
         <h2>To-Do List</h2> 
         <input></input> {" "}
         <button>Add</button> {" "}
-        <a>Clear</a>
+        <a>Clear</a><br/>
+        <p></p>
+        <div ref={divDisplay}>
+            
+        </div>
       </>
     );
   }
