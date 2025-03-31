36682733b6896905c7bf2bfbf5e4bf7c1c3bd081
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-31 09:51:22 -0400

    Contact Us page added and edited

---
+++ taskmgr-app/src/components/ContactUs.js
@@ -0,0 +1,27 @@
+function ContactUs() {
+    return (
+        <>
+        <div>  
+          <h2>Contact Us</h2>
+          <div></div>
+          <br/>
+          <form>
+              <div> 
+                  <span>* First name: </span> <input type="text" maxLength={25} placeholder="* First name"/>
+                  <br/>
+                  <span>Last name: </span> <input type="text" maxLength={25} placeholder="Last name"/>
+                  <br/>
+                  <span>* Email: </span> <input type="text" maxLength={100} placeholder="* Email"/>
+                  <br/>
+                  <span>* Comment: </span> 
+                  <br/><textarea rows={5} cols={30} maxLength={100} placeholder="* Comment"></textarea>
+                  <p></p>
+                  <button>Submit</button> {" "} <a>Clear</a>
+              </div>
+          </form>
+      </div>
+      </>
+    )
+}
+
+export default ContactUs;
