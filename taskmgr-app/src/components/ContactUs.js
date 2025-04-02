6ae9748a69cd83caa50baa350f32b5b6a1660a40
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-02 13:37:30 -0400

    contact us edited

--- taskmgr-app/src/components/ContactUs.js
+++ taskmgr-app/src/components/ContactUs.js
@@ -1,20 +1,25 @@
+
+
 function ContactUs() {
+
     return (
         <>
         <div className="div-main-contact">  
           <h2>Contact Us</h2>
-          <div></div>
+          <div>
+
+          </div>
           <br/>
           <form>
               <div> 
-                  <span>* First name: </span> <input type="text" maxLength={25} placeholder="* First name"/>
+                  <span>* First name: </span> <input type="text" maxLength={25} placeholder="* First name" required/>
                   <br/>
                   <span>Last name: </span> <input type="text" maxLength={25} placeholder="Last name"/>
                   <br/>
-                  <span>* Email: </span> <input type="text" maxLength={100} placeholder="* Email"/>
+                  <span>* Email: </span> <input type="text" maxLength={100} placeholder="* Email" required/>
                   <br/>
                   <span>* Comment: </span> 
-                  <br/><textarea rows={5} cols={30} maxLength={100} placeholder="* Comment"></textarea>
+                  <br/><textarea rows={5} cols={30} maxLength={100} placeholder="* Comment" required></textarea>
                   <p></p>
                   <button>Submit</button> {" "} <a>Clear</a>
               </div>
